dfd868c91e7d149171efb4acd31ea60f2537e892
Caiodev2 <caioadrean@gmail.com>
2026-09-23 17:39:52 -0300

    feat: initial backend

---
+++ form/backend/backend/src/test/java/caiodev2/form/api/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package caiodev2.form.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

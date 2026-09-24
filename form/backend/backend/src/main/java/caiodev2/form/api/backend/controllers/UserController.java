2c885e196082c7ef78868b234c879fa36137ec86
Caiodev2 <caioadrean@gmail.com>
2026-09-23 22:12:51 -0300

    feat: add UserController

---
+++ form/backend/backend/src/main/java/caiodev2/form/api/backend/controllers/UserController.java
@@ -0,0 +1,25 @@
+package caiodev2.form.api.backend.controllers;
+
+
+import caiodev2.form.api.backend.entities.User;
+import caiodev2.form.api.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User>  registerUser(@RequestBody User user){
+        User newUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+}

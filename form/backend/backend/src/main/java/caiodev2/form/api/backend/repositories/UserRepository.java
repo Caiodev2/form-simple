aec9d303a033f36258e740eff8188cbe55dd3f60
Caiodev2 <caioadrean@gmail.com>
2026-09-23 22:34:12 -0300

    config: disable CORS

--- form/backend/backend/src/main/java/caiodev2/form/api/backend/repositories/UserRepository.java
+++ form/backend/backend/src/main/java/caiodev2/form/api/backend/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package caiodev2.form.api.backend.repositories;
 import caiodev2.form.api.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }

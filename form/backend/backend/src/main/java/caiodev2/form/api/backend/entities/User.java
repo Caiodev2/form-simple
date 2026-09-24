26c7cfb7f87d1cb10c9576f5dde08ec2469c23bc
Caiodev2 <caioadrean@gmail.com>
2026-09-23 22:13:53 -0300

    feat: add table for user

--- form/backend/backend/src/main/java/caiodev2/form/api/backend/entities/User.java
+++ form/backend/backend/src/main/java/caiodev2/form/api/backend/entities/User.java
@@ -1,17 +1,14 @@
 package caiodev2.form.api.backend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
 
